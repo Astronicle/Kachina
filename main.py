6fe8a016532cda78630df94ab04c44ccf6f6312e
Abdul Rehman <astronicle78@gmail.com>
2025-05-08 12:12:38 +0530

    Enabled threading for Flask server

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def index():
 
 def run_flask():
     port = int(os.environ.get("PORT", 4000))  
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, threaded=True)
 
 # Start Flask in a background thread
 threading.Thread(target=run_flask).start()
